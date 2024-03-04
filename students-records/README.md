2e3943f3e275038a74b92e0b761d171c11810b16
Arshia-Zahedi <158775813+Arshia-mvp@users.noreply.github.com>
2024-03-05 01:54:46 +0330

    Add files via upload

---
+++ students-records/README.md
@@ -0,0 +1,2 @@
+# students-records
+hello my name is Arshia Zahedi. this is my project javascript.
---
+++ students-records/js/app.js
@@ -0,0 +1,61 @@
+alert('hello to all ، i made a report card of my friends');
+alert('I hope you like it.');
+let userName = ['Amir hossain Ardani' , 'Arshia Zahedi' , 'Amir ali Zahedi' , 'Taha Zanghaneh' , 'Mahdi Zanghaneh' , 'Pouya Fakhri'];
+console.log(userName);
+let userList = [
+    AmirhossainArdani = {
+        name:'Amir hossain',
+        lastName:'Ardani',
+        age:'14',
+        classActivity:'C-',
+        hanDout:'C-',
+        exAm:'C-',
+        oveRal:'C-',
+    },
+    ArshiaZahedi = {
+        name:'Arshia',
+        lastName:'Zahedi',
+        age:'15',
+        classActivity:'B-',
+        hanDout:'B-',
+        exAm:'B-',
+        oveRal:'B-',
+    },
+    AmiraliZahedi = {
+        name:'Amir ali',
+        lastName:'Zahedi',
+        age:'16',
+        classActivity:'B-',
+        hanDout:'C+',
+        exAm:'C+',
+        oveRal:'C+',
+    },
+    TahaZanghaneh = {
+        name:'Taha',
+        lastName:'Zanghaneh',
+        age:'12',
+        classActivity:'B-',
+        hanDout:'B-',
+        exAm:'B-',
+        oveRal:'B-',
+    },
+    MahdiZanghaneh = {
+        name:'Mahdi',
+        lastName:'Zanghaneh',
+        age:'14',
+        classActivity:'B-',
+        hanDout:'B-',
+        exAm:'B+',
+        oveRal:'B+',
+    },
+    PouyaFakhri = {
+        name:'Pouya',
+        lastName:'Fakhri',
+        age:'14',
+        classActivity:'A+',
+        hanDout:'B+',
+        exAm:'A-',
+        oveRal:'A-',
+    },
+]
+console.log(userList);
